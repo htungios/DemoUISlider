f65e35d61316bd5e6c12f96c1218161d4bb2e82a
Hoang Ngoc Tung <tungmdc55@gmail.com>
2015-10-31 09:34:49 +0700

    Initial Commit

---
+++ DemoUISlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoUISlider
+//
+//  Created by Hoang Ngoc Tung on 31/10/2015.
+//  Copyright (c) Năm 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoUISlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoUISlider
+//
+//  Created by Hoang Ngoc Tung on 31/10/2015.
+//  Copyright (c) Năm 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
